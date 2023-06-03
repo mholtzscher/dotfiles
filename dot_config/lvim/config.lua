1e821bcb7051f02a8a510de5bc18bd9a46f1b947
Michael Holtzscher <thenerdproject1@gmail.com>
2023-06-02 20:38:53 -0500

    Update .config/lvim/config.lua

--- dot_config/lvim/config.lua
+++ dot_config/lvim/config.lua
@@ -28,7 +28,8 @@ lvim.plugins = {
       require("leap").add_default_mappings()
     end,
   },
-  { "olexsmir/gopher.nvim" },
+  { "stevearc/dressing.nvim" },
+  -- { "olexsmir/gopher.nvim" },
   {
     "folke/trouble.nvim",
     cmd = "TroubleToggle",
@@ -45,27 +46,28 @@ lvim.plugins = {
       require("todo-comments").setup()
     end,
   },
-  {
-    "rmagatti/goto-preview",
-    config = function()
-      require('goto-preview').setup {
-        width = 120,             -- Width of the floating window
-        height = 25,             -- Height of the floating window
-        default_mappings = true, -- Bind default mappings
-        debug = false,           -- Print debug information
-        opacity = nil,           -- 0-100 opacity level of the floating window where 100 is fully transparent.
-        post_open_hook = nil     -- A function taking two arguments, a buffer and a window to be ran as a hook.
-        -- You can use "default_mappings = true" setup option
-        -- Or explicitly set keybindings
-        -- vim.cmd("nnoremap gpd <cmd>lua require('goto-preview').goto_preview_definition()<CR>")
-        -- vim.cmd("nnoremap gpi <cmd>lua require('goto-preview').goto_preview_implementation()<CR>")
-        -- vim.cmd("nnoremap gP <cmd>lua require('goto-preview').close_all_win()<CR>")
-      }
-    end
-  },
-  { "AckslD/swenv.nvim" },
-  { "stevearc/dressing.nvim" }
-
+  -- {
+  --   "rmagatti/goto-preview",
+  --   config = function()
+  --     require('goto-preview').setup {
+  --       width = 120,             -- Width of the floating window
+  --       height = 25,             -- Height of the floating window
+  --       default_mappings = true, -- Bind default mappings
+  --       debug = false,           -- Print debug information
+  --       opacity = nil,           -- 0-100 opacity level of the floating window where 100 is fully transparent.
+  --       post_open_hook = nil     -- A function taking two arguments, a buffer and a window to be ran as a hook.
+  --       -- You can use "default_mappings = true" setup option
+  --       -- Or explicitly set keybindings
+  --       -- vim.cmd("nnoremap gpd <cmd>lua require('goto-preview').goto_preview_definition()<CR>")
+  --       -- vim.cmd("nnoremap gpi <cmd>lua require('goto-preview').goto_preview_implementation()<CR>")
+  --       -- vim.cmd("nnoremap gP <cmd>lua require('goto-preview').close_all_win()<CR>")
+  --     }
+  --   end
+  -- },
+  -- { "AckslD/swenv.nvim" },
+  -- { "mfussenegger/nvim-dap-python" },
+  -- { "nvim-neotest/neotest" },
+  -- { "nvim-neotest/neotest-python" },
 }
 
 ------------------------
@@ -104,7 +106,7 @@ linters.setup {
 }
 
 ------------------------
--- Dap
+-- Go Dap
 ------------------------
 local dap_ok, dapgo = pcall(require, "dap-go")
 if not dap_ok then
@@ -150,22 +152,22 @@ lsp_manager.setup("gopls", {
   on_attach = function(client, bufnr)
     require("lvim.lsp").common_on_attach(client, bufnr)
     local _, _ = pcall(vim.lsp.codelens.refresh)
-    local map = function(mode, lhs, rhs, desc)
-      if desc then
-        desc = desc
-      end
+    -- local map = function(mode, lhs, rhs, desc)
+    --   if desc then
+    --     desc = desc
+    --   end
 
-      vim.keymap.set(mode, lhs, rhs, { silent = true, desc = desc, buffer = bufnr, noremap = true })
-    end
-    map("n", "<leader>Ci", "<cmd>GoInstallDeps<Cr>", "Install Go Dependencies")
-    map("n", "<leader>Ct", "<cmd>GoMod tidy<cr>", "Tidy")
-    map("n", "<leader>Ca", "<cmd>GoTestAdd<Cr>", "Add Test")
-    map("n", "<leader>CA", "<cmd>GoTestsAll<Cr>", "Add All Tests")
-    map("n", "<leader>Ce", "<cmd>GoTestsExp<Cr>", "Add Exported Tests")
-    map("n", "<leader>Cg", "<cmd>GoGenerate<Cr>", "Go Generate")
-    map("n", "<leader>Cf", "<cmd>GoGenerate %<Cr>", "Go Generate File")
-    map("n", "<leader>Cc", "<cmd>GoCmt<Cr>", "Generate Comment")
-    map("n", "<leader>DT", "<cmd>lua require('dap-go').debug_test()<cr>", "Debug Test")
+    --   vim.keymap.set(mode, lhs, rhs, { silent = true, desc = desc, buffer = bufnr, noremap = true })
+    -- end
+    -- map("n", "<leader>Ci", "<cmd>GoInstallDeps<Cr>", "Install Go Dependencies")
+    -- map("n", "<leader>Ct", "<cmd>GoMod tidy<cr>", "Tidy")
+    -- map("n", "<leader>Ca", "<cmd>GoTestAdd<Cr>", "Add Test")
+    -- map("n", "<leader>CA", "<cmd>GoTestsAll<Cr>", "Add All Tests")
+    -- map("n", "<leader>Ce", "<cmd>GoTestsExp<Cr>", "Add Exported Tests")
+    -- map("n", "<leader>Cg", "<cmd>GoGenerate<Cr>", "Go Generate")
+    -- map("n", "<leader>Cf", "<cmd>GoGenerate %<Cr>", "Go Generate File")
+    -- map("n", "<leader>Cc", "<cmd>GoCmt<Cr>", "Generate Comment")
+    -- map("n", "<leader>DT", "<cmd>lua require('dap-go').debug_test()<cr>", "Debug Test")
   end,
   on_init = require("lvim.lsp").common_on_init,
   capabilities = require("lvim.lsp").common_capabilities(),
@@ -183,20 +185,20 @@ lsp_manager.setup("gopls", {
   },
 })
 
-local status_ok, gopher = pcall(require, "gopher")
-if not status_ok then
-  return
-end
+-- local status_ok, gopher = pcall(require, "gopher")
+-- if not status_ok then
+--   return
+-- end
 
-gopher.setup {
-  commands = {
-    go = "go",
-    gomodifytags = "gomodifytags",
-    gotests = "gotests",
-    impl = "impl",
-    iferr = "iferr",
-  },
-}
+-- gopher.setup {
+--   commands = {
+--     go = "go",
+--     gomodifytags = "gomodifytags",
+--     gotests = "gotests",
+--     impl = "impl",
+--     iferr = "iferr",
+--   },
+-- }
 
 --------------------------------------
 -- Trouble Plugin Configuration
@@ -218,3 +220,52 @@ lvim.builtin.which_key.mappings["C"] = {
   name = "Python",
   c = { "<cmd>lua require('swenv.api').pick_venv()<cr>", "Choose Env" },
 }
+
+--------------------------------------
+-- Pyright Plugin Configuration
+--------------------------------------
+local pyright_opts = {
+  single_file_support = true,
+  settings = {
+    pyright = {
+      disableLanguageServices = false,
+      disableOrganizeImports = false
+    },
+    python = {
+      analysis = {
+        autoImportCompletions = true,
+        autoSearchPaths = true,
+        diagnosticMode = "workspace", -- openFilesOnly, workspace
+        typeCheckingMode = "basic",   -- off, basic, strict
+        useLibraryCodeForTypes = true
+      }
+    }
+  },
+}
+
+require("lvim.lsp.manager").setup("pyright", pyright_opts)
+
+--------------------------------------
+-- Jsonls Plugin Configuration
+--------------------------------------
+local json_opts = {
+  settings = {
+    json = {
+      schemas = vim.list_extend(
+        {
+          {
+            description = "pyright config",
+            fileMatch = { "pyrightconfig.json" },
+            name = "pyrightconfig.json",
+            url =
+            "https://raw.githubusercontent.com/microsoft/pyright/main/packages/vscode-pyright/schemas/pyrightconfig.schema.json",
+          },
+        },
+        require('schemastore').json.schemas {
+        }
+      ),
+    },
+  },
+}
+
+require("lvim.lsp.manager").setup("jsonls", json_opts)
