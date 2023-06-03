bc143f58dd5f5c9274330dcef19cd499ddaf5cb1
Michael Holtzscher <thenerdproject1@gmail.com>
2023-06-02 21:04:33 -0500

    Update .config/lvim/config.lua

--- dot_config/lvim/config.lua
+++ dot_config/lvim/config.lua
@@ -68,6 +68,17 @@ lvim.plugins = {
   -- { "mfussenegger/nvim-dap-python" },
   -- { "nvim-neotest/neotest" },
   -- { "nvim-neotest/neotest-python" },
+  {
+    "pwntester/octo.nvim",
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-telescope/telescope.nvim',
+      'nvim-tree/nvim-web-devicons',
+    },
+    config = function()
+      require("octo").setup()
+    end,
+  },
 }
 
 ------------------------
