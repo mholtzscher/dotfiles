e562de9305e7bbc4f4302f8d99594a4fcf846c18
Michael Holtzscher <michael@holtzscher.com>
2023-10-09 08:44:51 -0500

    Update .config/lvim/config.lua

--- dot_config/lvim/config.lua
+++ dot_config/lvim/config.lua
@@ -120,7 +120,6 @@ lvim.plugins = {
       "nvim-treesitter/nvim-treesitter",
       "antoinemadec/FixCursorHold.nvim",
       "nvim-neotest/neotest-go",
-      "sidlatau/neotest-dart",
     },
     keys = {
       {
@@ -215,7 +214,6 @@ lvim.plugins = {
     dependencies = {
       'nvim-lua/plenary.nvim',
       'stevearc/dressing.nvim', -- optional for vim.ui.select
-      "RobertBrunhage/flutter-riverpod-snippets",
     },
     config = function()
       require("flutter-tools").setup {
@@ -519,10 +517,10 @@ require("noice").setup({
   },
   -- you can enable a preset for easier configuration
   presets = {
-    bottom_search = true,         -- use a classic bottom cmdline for search
+    -- bottom_search = true,         -- use a classic bottom cmdline for search
     command_palette = true,       -- position the cmdline and popupmenu together
     long_message_to_split = true, -- long messages will be sent to a split
     inc_rename = false,           -- enables an input dialog for inc-rename.nvim
-    lsp_doc_border = false,       -- add a border to hover docs and signature help
+    lsp_doc_border = true,        -- add a border to hover docs and signature help
   },
 })
