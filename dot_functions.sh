fe525f791585b10acc42a0fcb6c414d4dabeee6f
Michael Holtzscher <michael@holtzscher.com>
2024-04-05 16:37:08 -0500

    Update .functions.sh

--- dot_functions.sh
+++ dot_functions.sh
@@ -28,10 +28,16 @@ function galactus() {
 		--align center --width 50 --margin "1 2" --padding "2 4" \
 		'Behold Galactus, the Devourer of Worlds!'
 	gum format "Choose your configuration weapon:"
-	CATEGORY=$(gum choose "all" "brew" "mas" "npm" "starship" "xcode" "sdkman")
+	CATEGORY=$(gum choose "all" "brew" "go" "mas" "starship" "xcode" "sdkman")
 
 	if [[ $CATEGORY == "brew" ]] || [[ $CATEGORY == "all" ]]; then
 		gum spin --spinner moon --title "Installing Homebrew Bundle..." -- brew bundle --no-lock --file=~/Brewfile
+	fi
+
+	if [[ $CATEGORY == "go" ]] || [[ $CATEGORY == "all" ]]; then
+		gum spin --spinner moon --title "Installing protoc-gen-gotag..." -- go install github.com/srikrsna/protoc-gen-gotag@latest
+		gum spin --spinner moon --title "Installing godotenv..." -- go install github.com/joho/go-dotenv/cmd/godotenv@latest
+		gum spin --spinner moon --title "Installing govulncheck" -- go install golang.org/x/vuln/cmd/govulncheck@latest
 	fi
 
 	if [[ $CATEGORY == "mas" ]] || [[ $CATEGORY == "all" ]]; then
