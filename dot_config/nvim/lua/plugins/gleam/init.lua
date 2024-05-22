d41f52f50644e4a42b5f37e204fac19086420010
Michael Holtzscher <michael@holtzscher.com>
2024-05-21 22:22:39 -0500

    Update .config/nvim/lazyvim.json Update .config/nvim/lua/plugins/gleam/init.lua

--- dot_config/nvim/lua/plugins/gleam/init.lua
+++ dot_config/nvim/lua/plugins/gleam/init.lua
@@ -1,16 +1,24 @@
 return {
+  recommended = function()
+    return LazyVim.extras.wants({
+      ft = "gleam",
+      root = { "gleam.toml" },
+    })
+  end,
   {
     "neovim/nvim-lspconfig",
-    config = function()
-      require("lspconfig").gleam.setup({})
-    end,
+    opts = {
+      servers = {
+        gleam = {},
+      },
+    },
   },
   {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, {
-        "gleam",
-      })
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "gleam" })
+      end
     end,
   },
 }
