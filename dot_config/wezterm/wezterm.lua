16f3c78a36a5b12fd94a28c40f86a9b3b82a6cf0
Michael Holtzscher <michael@holtzscher.com>
2024-05-30 21:14:29 -0500

    Add .config/wezterm/wezterm.lua

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,17 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- Appearance
+config.color_scheme = "Catppuccin Mocha"
+config.hide_tab_bar_if_only_one_tab = true
+config.font = wezterm.font("Iosevka Nerd Font Mono")
+config.font_size = 13.0
+
+config.initial_cols = 200
+config.initial_rows = 50
+
+-- and finally, return the configuration to wezterm
+return config
