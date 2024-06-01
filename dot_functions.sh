5c6d807cfc543fd7eb5bec75d0c320ac2eb79063
Michael Holtzscher <michael@holtzscher.com>
2024-05-31 22:46:34 -0500

    Update .functions.sh

--- dot_functions.sh
+++ dot_functions.sh
@@ -17,41 +17,69 @@ function galactus() {
 	CATEGORY=$(gum choose "all" "asdf" "brew" "go" "mas" "tpm" "xcode" "zinit")
 
 	if [[ $CATEGORY == "asdf" ]] || [[ $CATEGORY == "all" ]]; then
-		gum spin --spinner moon --show-output --title "Installing asdf plugin java..." -- asdf plugin add java
-		gum spin --spinner moon --show-output --title "Installing asdf plugin nodejs..." -- asdf plugin add nodejs
-		gum spin --spinner moon --show-output --title "Installing asdf plugin go..." -- asdf plugin add golang
-		gum spin --spinner moon --show-output --title "Installing asdf plugin terraform..." -- asdf plugin add terraform
-		gum spin --spinner moon --show-output --title "Installing asdf plugin rust..." -- asdf plugin add rust
-		gum spin --spinner moon --show-output --title "Installing asdf plugin gleam..." -- asdf plugin add gleam
-		gum spin --spinner moon --show-output --title "Installing asdf plugin erlang..." -- asdf plugin add erlang
-		gum spin --spinner moon --show-output --title "Updating asdf plugins..." -- asdf plugin update --all
+		echo "Installing asdf plugin java..."
+		asdf plugin add java
+
+		echo "Installing asdf plugin nodejs..."
+		asdf plugin add nodejs
+
+		echo "Installing asdf plugin go..."
+		asdf plugin add golang
+
+		echo "Installing asdf plugin terraform..."
+		asdf plugin add terraform
+
+		echo "Installing asdf plugin rust..."
+		asdf plugin add rust
+
+		echo "Installing asdf plugin gleam..."
+		asdf plugin add gleam
+
+		echo "Installing asdf plugin erlang..."
+		asdf plugin add erlang
+
+		echo "Updating asdf plugins..."
+		asdf plugin update --all
 	fi
 
 	if [[ $CATEGORY == "brew" ]] || [[ $CATEGORY == "all" ]]; then
-		gum spin --spinner moon --show-output --title "Installing Homebrew Bundle..." -- brew bundle --no-lock --file=~/Brewfile
+		echo "Installing Homebrew Bundle..."
+		brew bundle --no-lock --file=~/Brewfile
 	fi
 
 	if [[ $CATEGORY == "go" ]] || [[ $CATEGORY == "all" ]]; then
-		gum spin --spinner moon --show-output --title "Installing protoc-gen-gotag..." -- go install github.com/srikrsna/protoc-gen-gotag@latest
-		gum spin --spinner moon --show-output --title "Installing godotenv..." -- go install github.com/joho/go-dotenv/cmd/godotenv@latest
-		gum spin --spinner moon --show-output --title "Installing govulncheck" -- go install golang.org/x/vuln/cmd/govulncheck@latest
+		echo "Setting golang to latest..."
+		asdf global golang latest
+
+		echo "Installing protoc-gen-gotag..."
+		go install github.com/srikrsna/protoc-gen-gotag@latest
+
+		echo "Installing godotenv..."
+		go install github.com/joho/go-dotenv/cmd/godotenv@latest
+
+		echo "Installing govulncheck"
+		go install golang.org/x/vuln/cmd/govulncheck@latest
 	fi
 
 	if [[ $CATEGORY == "mas" ]] || [[ $CATEGORY == "all" ]]; then
-		gum spin --spinner moon --show-output --title "Installing Mac App Store apps..." -- brew bundle --no-lock --file=~/Brewfile-mas
+		echo "Installing Mac App Store apps..."
+		brew bundle --no-lock --file=~/Brewfile-mas
 	fi
 
 	if [[ $CATEGORY == "tpm" ]] || [[ $CATEGORY == "all" ]]; then
+		echo "Updating tpm plugins..."
 		~/.config/tmux/plugins/tpm/bin/update_plugins all
 	fi
 
 	if [[ $CATEGORY == "xcode" ]] || [[ $CATEGORY == "all" ]]; then
 		if ! xcode-select -p >/dev/null 2>&1; then
-			gum spin --spinner moon --show-output --title "Installing xcode-select..." -- xcode-select --install
+			echo "Installing xcode-select..."
+			xcode-select --install
 		fi
 	fi
 
 	if [[ $CATEGORY == "zinit" ]] || [[ $CATEGORY == "all" ]]; then
+		echo "Updating zinit plugins..."
 		zinit update
 	fi
 }
