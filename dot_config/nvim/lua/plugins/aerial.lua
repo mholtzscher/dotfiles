d7a3291d7d180c82b26a06befd3e5af608dcca55
Michael Holtzscher <michael@holtzscher.com>
2024-06-02 09:37:02 -0500

    Add .config/nvim/lua/plugins/aerial.lua

---
+++ dot_config/nvim/lua/plugins/aerial.lua
@@ -0,0 +1,9 @@
+return {
+  "stevearc/aerial.nvim",
+  opts = {
+    highlight_on_hover = true,
+    autojump = true,
+    open_automatic = true,
+    layout = { min_width = 50, default_direction = "prefer_left" },
+  },
+}
