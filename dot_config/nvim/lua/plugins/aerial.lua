42d42a0a472053bfc2e4e0ca0e2f272c09983aee
Michael Holtzscher <michael@holtzscher.com>
2024-06-30 22:05:20 -0500

    Update .config/nvim/lazyvim.json Update .config/nvim/lua/plugins/aerial.lua

--- dot_config/nvim/lua/plugins/aerial.lua
+++ dot_config/nvim/lua/plugins/aerial.lua
@@ -3,7 +3,7 @@ return {
   opts = {
     highlight_on_hover = true,
     autojump = true,
-    open_automatic = true,
+    -- open_automatic = true,
     layout = { min_width = 50, default_direction = "prefer_left" },
   },
 }
