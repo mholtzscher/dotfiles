376fecca6939a063c95a5386b0905e1522f84b8b
Michael Holtzscher <michael@holtzscher.com>
2024-08-05 09:42:48 -0500

    Update .config/fish/config.fish Update .config/fish/fish_variables Update .config/tmux/tmux.conf

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -3,7 +3,9 @@ if status is-interactive
     atuin init fish | source
 end
 
+fish_add_path "$ASDF_DIR/bin"
+fish_add_path "$ASDF_DIR/shims"
+source /opt/homebrew/opt/asdf/libexec/asdf.fish
+
 zoxide init fish | source
 starship init fish | source
-
-source /opt/homebrew/opt/asdf/libexec/asdf.fish
