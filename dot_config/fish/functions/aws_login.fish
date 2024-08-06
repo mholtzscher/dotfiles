33707b57c728e9bc55f26102f1acabce30389b6c
Michael Holtzscher <michael@holtzscher.com>
2024-08-05 21:10:05 -0500

    Remove .config/fish/functions/aws_login.fish

--- dot_config/fish/functions/aws_login.fish
+++
@@ -1,6 +0,0 @@
-function aws_login --description 'Login to AWS SSO and set the AWS_PROFILE'
-    set -l profile $argv[1]
-
-    aws sso login --profile $profile
-    set -gx AWS_PROFILE $profile
-end
