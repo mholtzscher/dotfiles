fdebc0db2c9832557341070cb8c41d4ec7d59d7a
Michael Holtzscher <michael@holtzscher.com>
2024-08-15 16:39:39 -0500

    Update .config/nvim/lua/plugins/java/init.lua Update .ideavimrc

--- dot_config/nvim/lua/plugins/java/init.lua
+++ dot_config/nvim/lua/plugins/java/init.lua
@@ -1,29 +1,53 @@
+-- return {
+--   "nvim-java/nvim-java",
+--   dependencies = {
+--     "nvim-java/lua-async-await",
+--     "nvim-java/nvim-java-refactor",
+--     "nvim-java/nvim-java-core",
+--     "nvim-java/nvim-java-test",
+--     "nvim-java/nvim-java-dap",
+--     "MunifTanjim/nui.nvim",
+--     "neovim/nvim-lspconfig",
+--     "mfussenegger/nvim-dap",
+--     {
+--       "williamboman/mason.nvim",
+--       opts = {
+--         registries = {
+--           "github:nvim-java/mason-registry",
+--           "github:mason-org/mason-registry",
+--         },
+--       },
+--     },
+--     {
+--       "williamboman/mason-lspconfig.nvim",
+--       opts = {
+--         handlers = {
+--           ["jdtls"] = function()
+--             require("java").setup()
+--           end,
+--         },
+--       },
+--     },
+--   },
+-- }
+
 return {
   "nvim-java/nvim-java",
+  config = false,
   dependencies = {
-    "nvim-java/lua-async-await",
-    "nvim-java/nvim-java-refactor",
-    "nvim-java/nvim-java-core",
-    "nvim-java/nvim-java-test",
-    "nvim-java/nvim-java-dap",
-    "MunifTanjim/nui.nvim",
-    "neovim/nvim-lspconfig",
-    "mfussenegger/nvim-dap",
     {
-      "williamboman/mason.nvim",
+      "neovim/nvim-lspconfig",
       opts = {
-        registries = {
-          "github:nvim-java/mason-registry",
-          "github:mason-org/mason-registry",
+        servers = {
+          jdtls = {
+            -- your jdtls configuration goes here
+          },
         },
-      },
-    },
-    {
-      "williamboman/mason-lspconfig.nvim",
-      opts = {
-        handlers = {
-          ["jdtls"] = function()
-            require("java").setup()
+        setup = {
+          jdtls = function()
+            require("java").setup({
+              -- your nvim-java configuration goes here
+            })
           end,
         },
       },
