dd71fea9b367348edef33c18af9c7e2d97db4750
Michael Holtzscher <michael@holtzscher.com>
2024-08-17 21:52:27 -0500

    Remove .config/nvim/lua/plugins/goplement.lua

--- dot_config/nvim/lua/plugins/goplement.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "maxandron/goplements.nvim",
-  ft = "go",
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  },
-}
