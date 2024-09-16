952907918f440f9f081b379141512e290b7dea20
Michael Holtzscher <michael@holtzscher.com>
2024-09-16 09:29:41 -0500

    Update .config/fish/functions/clean.fish

--- dot_config/fish/functions/clean.fish
+++ dot_config/fish/functions/clean.fish
@@ -1,4 +1,3 @@
-function clean --wraps='git clean -Xdf' --description 'alias clean=git clean -Xdf'
-  git clean -Xdf $argv
-        
+function clean --wraps='git clean -Xdf' --description 'Remove files that are in gitignore'
+    git clean -Xdf $argv
 end
