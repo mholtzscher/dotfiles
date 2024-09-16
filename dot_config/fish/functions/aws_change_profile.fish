8cd39a196718ec521d14b5811c14a5d569b846c1
Michael Holtzscher <michael@holtzscher.com>
2024-09-16 13:57:57 -0500

    Update .config/fish/functions/aws_change_profile.fish

--- dot_config/fish/functions/aws_change_profile.fish
+++ dot_config/fish/functions/aws_change_profile.fish
@@ -1,13 +1,25 @@
-#function aws_login --description 'Login to AWS SSO and set the AWS_PROFILE'
-#    set -l profile $argv[1]
-#
-#    aws sso login --profile $profile
-#    set -gx AWS_PROFILE $profile
-#end
-
 function aws_change_profile --description 'Change the AWS profile and login to SSO'
-    set -gx AWS_PROFILE $argv[1]
-    echo "Using AWS profile: $AWS_PROFILE"
+    set -l profile
+
+    if test -z "$argv[1]"
+        if command -v fzf >/dev/null 2>&1
+            set profile (aws configure list-profiles | fzf)
+            and set -gx AWS_PROFILE "$profile"
+        else
+            echo "fzf is not installed. Please install fzf to use this feature."
+            return 1
+        end
+    else
+        set profile $argv[1]
+    end
+
+    if test -z "$profile"
+        echo "No profile selected"
+        return 1
+    end
+
+    set -gx AWS_PROFILE "$profile"
+    echo -e "Using AWS profile: $AWS_PROFILE"
 
     if aws configure get sso_start_url --profile $AWS_PROFILE >/dev/null 2>&1
         aws_sso_login
