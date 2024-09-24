28e115fbd36b82ab39909a468825c13fe9d3c7e9
Michael Holtzscher <michael@holtzscher.com>
2024-09-23 20:47:09 -0500

    Add .config/fish/functions/sops_production.fish

---
+++ dot_config/fish/functions/sops_production.fish
@@ -0,0 +1,6 @@
+function sops_staging --description "Run sops with the production profile"
+    set -l AWS_PROFILE m3p_staging
+    set -l SOPS_KMS_ARN "arn:aws:kms:us-west-2:211125772151:key/mrk-d167c0b6c99945fabfc4b629d52450ad"
+
+    command sops $argv
+end
