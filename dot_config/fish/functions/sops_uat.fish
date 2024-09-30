79400df9038e4cf4b3c8af901c9c0b00d00ca499
Michael Holtzscher <michael@holtzscher.com>
2024-09-30 07:44:36 -0500

    Update .config/fish/functions/sops_uat.fish

--- dot_config/fish/functions/sops_uat.fish
+++ dot_config/fish/functions/sops_uat.fish
@@ -1,6 +1,6 @@
-function sops_staging --description "Run sops with the uat profile"
-    set -l AWS_PROFILE m3p_staging
-    set -l SOPS_KMS_ARN "arn:aws:kms:us-west-2:590183679435:key/mrk-3c092342ff9a488399c0ffee8e89eb53"
+function sops_uat --description "Run sops with the uat profile"
+    set -x AWS_PROFILE m3p_uat
+    set -x SOPS_KMS_ARN "arn:aws:kms:us-west-2:590183679435:key/mrk-3c092342ff9a488399c0ffee8e89eb53"
 
     command sops $argv
 end
