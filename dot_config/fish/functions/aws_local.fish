9fc46a700a68a63d5f9681639e1a93f116e104d1
Michael Holtzscher <michael@holtzscher.com>
2024-10-10 08:26:04 -0500

    Update .config/fish/functions/aws_local.fish

--- dot_config/fish/functions/aws_local.fish
+++ dot_config/fish/functions/aws_local.fish
@@ -1,3 +1,3 @@
-function awslocal --description 'Run AWS CLI commands against LocalStack'
+function aws_local --description 'Run AWS CLI commands against LocalStack'
     env AWS_PROFILE=localstack aws --endpoint-url=http://localhost.localstack.cloud:4566 $argv
 end
