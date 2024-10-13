93fde589ddf1f1c86a48e172c17c6d5195911e66
Michael Holtzscher <michael@holtzscher.com>
2024-10-13 18:16:54 -0500

    Update .config/nix-darwin/home.nix

--- dot_config/nix-darwin/home.nix
+++ dot_config/nix-darwin/home.nix
@@ -152,6 +152,41 @@
       };
     };
 
+    k9s = {
+      enable = true;
+      settings = {
+        k9s = {
+          ui = {
+            skin = "catppuccin-mocha";
+          };
+        };
+      };
+      skins = {
+        catppuccin-mocha = ./themes/k9s-catppuccin-mocha.yaml;
+      };
+    };
+
+    lazygit = {
+      enable = true;
+      settings = {
+        gui.theme = {
+          activeBorderColor = [
+            "#f5c2e7"
+            "bold"
+          ];
+          inactiveBorderColor = [ "#a6adc8" ];
+          optionsTextColor = [ "#89b4fa" ];
+          selectedLineBgColor = [ "#313244" ];
+          selectedRangeBgColor = [ "#313244" ];
+          cherryPickedCommitBgColor = [ "#45475a" ];
+          cherryPickedCommitFgColor = [ "#f5c2e7" ];
+          unstagedChangesColor = [ "#f38ba8" ];
+          defaultFgColor = [ "#cdd6f4" ];
+          searchingActiveBorderColor = [ "#f9e2af" ];
+        };
+      };
+    };
+
     # fish = {
     #   enable = true;
     #   # enableCompletions = true;
