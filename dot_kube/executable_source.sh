c9e4f682df4f932fe5322a07fd4c3c024082faaf
Michael Holtzscher <michael@holtzscher.com>
2024-10-19 17:53:21 -0500

    Remove .kube/source.sh

--- dot_kube/executable_source.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-OIFS="$IFS"
-IFS=$'\n'
-for kubeconfigFile in `find "$HOME/.kube" -type f -name "*.yml" -o -name "*.yaml"`
-do
-    export KUBECONFIG="$kubeconfigFile:$KUBECONFIG"
-done
-IFS="$OIFS"
