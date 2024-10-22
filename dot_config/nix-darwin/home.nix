0441d40132a8ffcd753c7cc993904c0af09c4dc5
Michael Holtzscher <michael@holtzscher.com>
2024-10-21 22:57:52 -0500

    Update .config/nix-darwin/home.nix

--- dot_config/nix-darwin/home.nix
+++ dot_config/nix-darwin/home.nix
@@ -199,10 +199,8 @@
     fish = {
       enable = true;
       shellAbbrs = {
-        sso = "aws_change_profile";
         build = "gradle build --parallel";
-        nu = "darwin-rebuild switch --flake ~/.config/nix-darwin";
-
+        sso = "aws_change_profile";
       };
       interactiveShellInit = ''
         fish_add_path "$ASDF_DIR/bin"
@@ -216,6 +214,68 @@
             set -gx KUBECONFIG "$kubeconfigFile:$KUBECONFIG"
         end
       '';
+      functions = {
+        gitignore = {
+          body = "curl -sL https://www.gitignore.io/api/$argv";
+          description = "get gitgnore for language";
+        };
+        ip = {
+          body = "dig +short myip.opendns.com @resolver1.opendns.com $argv";
+          wraps = "dig +short myip.opendns.com @resolver1.opendns.com";
+          description = "get public ip address";
+        };
+        ips = {
+          body = ''
+            ifconfig -a | grep -o 'inet6\? \(addr:\)\?\s\?\(\(\([0-9]\+\.\)\{3\}[0-9]\+\)\|[a-fA-F0-9:]\+\)' | awk '{ sub(/inet6? (addr:)? ?/, ""); print }' $argv
+          '';
+          description = "Show all ip addresses for machine";
+        };
+        lg = {
+          body = "lazygit $argv";
+          description = "lazygit";
+          wraps = "lazygit";
+        };
+        ll = {
+          body = "lsd -al $argv";
+          description = "lsd -al";
+          wraps = "lsd";
+        };
+        localip = {
+          body = "ipconfig getifaddr en0 $argv";
+          description = "get local ip address";
+        };
+        nu = {
+          body = "darwin-rebuild switch --flake ~/.config/nix-darwin";
+          description = "Rebuild nix-darwin";
+        };
+        pbj = {
+          body = "pbpaste | jq $argv";
+          description = "pretty print json from clipboard";
+        };
+        show = {
+          body = "defaults write com.apple.finder AppleShowAllFiles -bool true && killall Finder";
+          description = "show hidden files in finder";
+        };
+        tf = {
+          body = "terraform $argv";
+          description = "terraform";
+          wraps = "terraform";
+        };
+        tree = {
+          body = "lsd --tree -a --ignore-glob .git $argv";
+          description = "pretty tree view of all files";
+          wraps = "lsd";
+        };
+        treed = {
+          body = "lsd --tree -a -d --ignore-glob .git --ignore-glob gen $argv";
+          description = "pretty tree view of only directories";
+          wraps = "lsd";
+        };
+        weather = {
+          body = "curl wttr.in $argv";
+          description = "get weather";
+        };
+      };
     };
 
     wezterm = {
