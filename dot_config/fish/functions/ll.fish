86bc73d6c40bbe32677b0e4ef6bde3ea72932ead
Michael Holtzscher <michael@holtzscher.com>
2024-10-21 22:57:29 -0500

    Remove .config/fish/functions/lg.fish Remove .config/fish/functions/ll.fish Remove .config/fish/functions/localip.fish Remove .config/fish/functions/pbj.fish Remove .config/fish/functions/show.fish Remove .config/fish/functions/tf.fish Remove .config/fish/functions/tree.fish Remove .config/fish/functions/treed.fish Remove .config/fish/functions/weather.fish Remove .config/fish/functions/work-add.fish Remove .config/fish/functions/work-del.fish Remove .config/fish/functions/work.fish

--- dot_config/fish/functions/ll.fish
+++
@@ -1,4 +0,0 @@
-function ll --wraps='lsd -al' --description 'alias ll=lsd -al'
-  lsd -al $argv
-        
-end
--- dot_config/fish/functions/work-add.fish
+++
@@ -1,9 +0,0 @@
-function work-add --description "Add a git worktree"
-    if test (count $argv) -ne 1
-        echo "Usage: work-add <branch-name>"
-        return 1
-    end
-
-    set branch_name $argv[1]
-    git worktree add $branch_name
-end
