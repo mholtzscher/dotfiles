1358948434d161ccf40c48db2856a984bb7760a9
Michael Holtzscher <michael@holtzscher.com>
2024-10-21 23:26:33 -0500

    Update .config/fish/fish_variables Update .config/nix-darwin/home.nix

--- dot_config/nix-darwin/home.nix
+++ dot_config/nix-darwin/home.nix
@@ -212,6 +212,13 @@
         for kubeconfigFile in (fd -e yml -e yaml . "$HOME/.kube")
             set -gx KUBECONFIG "$kubeconfigFile:$KUBECONFIG"
         end
+
+        set -Ux FZF_DEFAULT_OPTS "\
+        --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+        --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+        --color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
+        --color=selected-bg:#45475a \
+        --multi"
       '';
 
       functions = {
@@ -363,6 +370,12 @@
       '';
       sessionVariables = {
         PATH = "$HOME/go/bin:$PATH";
+        FZF_DEFAULT_OPTS = " \
+            --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+            --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+            --color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
+            --color=selected-bg:#45475a \
+            --multi";
       };
       antidote = {
         enable = true;
