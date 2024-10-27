bd1213300f421040d506e08988bd0ee8b51015ea
Michael Holtzscher <michael@holtzscher.com>
2024-10-27 17:58:22 -0500

    Add .config/nix-darwin/modules/home-manager/programs/default.nix

---
+++ dot_config/nix-darwin/modules/home-manager/programs/default.nix
@@ -0,0 +1,19 @@
+{ ... }:
+{
+  imports = [
+    ./atuin.nix
+    ./bat.nix
+    ./bottom.nix
+    ./fd.nix
+    ./fzf.nix
+    ./git.nix
+    ./k9s.nix
+    ./lazygit.nix
+    ./fish.nix
+    ./starship.nix
+    ./thefuck.nix
+    ./wezterm.nix
+    ./zsh.nix
+    ./zoxide.nix
+  ];
+}
