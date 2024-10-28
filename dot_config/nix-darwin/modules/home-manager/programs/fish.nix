d9ae2297a8af48271cdada757ac62993ae359bc1
Michael Holtzscher <michael@holtzscher.com>
2024-10-27 22:05:08 -0500

    Update .config/nix-darwin/modules/home-manager/programs/fish.nix

--- dot_config/nix-darwin/modules/home-manager/programs/fish.nix
+++ dot_config/nix-darwin/modules/home-manager/programs/fish.nix
@@ -40,6 +40,18 @@
           description = "Login to AWS SSO";
         };
 
+        __ssh_tunnel = {
+          body = ''
+            set -l KEY_FILE_PATH $argv[1]
+            set -l LOCAL_PORT $argv[2]
+            set -l ENDPOINT $argv[3]
+            set -l USER_HOSTNAME $argv[4]
+
+            ssh -i $KEY_FILE_PATH -v -N -L $LOCAL_PORT:$ENDPOINT $USER_HOSTNAME
+          '';
+          description = "Create an SSH tunnel";
+        };
+
         aws_change_profile = {
           body = ''
             set -l profile
