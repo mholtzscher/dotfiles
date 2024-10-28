04d119d0cec0a299ee19a025aa579fa80a534d75
Michael Holtzscher <michael@holtzscher.com>
2024-10-27 21:24:53 -0500

    Remove .config/fish/functions/gradle.fish

--- dot_config/fish/functions/gradle.fish
+++
@@ -1,7 +0,0 @@
-function gradle --wraps=./gradlew --description 'swaps ./gradlew for gradle'
-    if test -e ./gradlew
-        ./gradlew $argv
-    else
-        echo "No gradlew found"
-    end
-end
