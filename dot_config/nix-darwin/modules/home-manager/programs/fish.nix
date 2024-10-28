626789bb369517a85eaf96d572c853144f14e3c9
Michael Holtzscher <michael@holtzscher.com>
2024-10-27 21:57:16 -0500

    Update .config/nix-darwin/modules/home-manager/programs/fish.nix

--- dot_config/nix-darwin/modules/home-manager/programs/fish.nix
+++ dot_config/nix-darwin/modules/home-manager/programs/fish.nix
@@ -28,6 +28,48 @@
       '';
 
       functions = {
+        __aws_sso_login = {
+          body = ''
+            if aws sts get-caller-identity >/dev/null 2>&1
+                echo "Found valid AWS session"
+            else
+                echo "Logging into AWS"
+                aws sso login
+            end
+          '';
+          description = "Login to AWS SSO";
+        };
+
+        aws_change_profile = {
+          body = ''
+            set -l profile
+
+            if test -z $argv[1]
+                if command -v fzf >/dev/null 2>&1
+                    set profile (aws configure list-profiles | fzf)
+                    and set -gx AWS_PROFILE "$profile"
+                else
+                    echo "fzf is not installed. Please install fzf to use this feature."
+                    return 1
+                end
+            else
+                set profile $argv[1]
+            end
+
+            if test -z "$profile"
+                echo "No profile selected"
+                return 1
+            end
+
+            set -gx AWS_PROFILE "$profile"
+            echo -e "Using AWS profile: $AWS_PROFILE"
+
+            if aws configure get sso_start_url --profile $AWS_PROFILE >/dev/null 2>&1
+                __aws_sso_login
+            end
+          '';
+          description = "Change the AWS profile and login to SSO";
+        };
         aws_ecr_login = {
           body = "aws ecr get-login-password | docker login --username AWS --password-stdin 188442536245.dkr.ecr.us-west-2.amazonaws.com";
           description = "Login to AWS ECR";
