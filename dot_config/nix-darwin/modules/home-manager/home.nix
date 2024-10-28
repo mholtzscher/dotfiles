9447fbc1faf70c468563c6843aec0f82892dcc0d
Michael Holtzscher <michael@holtzscher.com>
2024-10-27 23:06:48 -0500

    Update .config/nix-darwin/modules/home-manager/home.nix Update .config/nix-darwin/modules/home-manager/programs/fish.nix

--- dot_config/nix-darwin/modules/home-manager/home.nix
+++ dot_config/nix-darwin/modules/home-manager/home.nix
@@ -24,7 +24,7 @@
       }
       + "/themes/Catppuccin Mocha.theme";
     "${config.xdg.configHome}/fish/functions/update.fish".source = ./files/fish/functions/update.fish;
-    "${config.xdg.configHome}/fish/fish_variables".source = ./files/fish_variables;
+    "${config.xdg.configHome}/fish/fish_variables".source = ./files/fish/fish_variables;
     "${config.xdg.configHome}/nvim" = {
       source = ./files/nvim;
       recursive = true;
