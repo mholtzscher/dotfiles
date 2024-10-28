696bd1b05979cc0f70a0c5042887245972bd3957
Michael Holtzscher <michael@holtzscher.com>
2024-10-27 21:29:37 -0500

    Remove .config/fish/functions/cacheclear.fish

--- dot_config/fish/functions/cacheclear.fish
+++
@@ -1,4 +0,0 @@
-function cacheclear --wraps='sudo dscacheutil -flushcache; sudo killall -HUP mDNSResponder' --description 'alias cacheclear=sudo dscacheutil -flushcache; sudo killall -HUP mDNSResponder'
-  sudo dscacheutil -flushcache; sudo killall -HUP mDNSResponder $argv
-        
-end
