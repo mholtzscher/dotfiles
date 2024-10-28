b5904ed8d109146fac40a2ebe0f9263aa003c8f3
Michael Holtzscher <michael@holtzscher.com>
2024-10-27 21:30:07 -0500

    Remove .config/fish/functions/hide.fish

--- dot_config/fish/functions/hide.fish
+++
@@ -1,4 +0,0 @@
-function hide --wraps='defaults write com.apple.finder AppleShowAllFiles -bool false && killall Finder' --description 'alias hide=defaults write com.apple.finder AppleShowAllFiles -bool false && killall Finder'
-  defaults write com.apple.finder AppleShowAllFiles -bool false && killall Finder $argv
-        
-end
