4fda8268ade7a1a2ce24a36c4502aa028c0329c9
Michael Holtzscher <michael@holtzscher.com>
2024-10-27 21:45:53 -0500

    Remove .config/fish/functions/aws_logout.fish

--- dot_config/fish/functions/aws_logout.fish
+++
@@ -1,7 +0,0 @@
-function aws_logout --description "Logout from AWS SSO"
-    if aws configure get sso_start_url --profile $AWS_PROFILE >/dev/null 2>&1
-        aws sso logout
-    end
-
-    set -e AWS_PROFILE
-end
