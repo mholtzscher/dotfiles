9351c12bd62feafaf441211766604554ecac8c82
Michael Holtzscher <michael@holtzscher.com>
2024-10-27 22:04:43 -0500

    Remove .config/fish/functions/aws_uat_m3p_db.fish

--- dot_config/fish/functions/aws_uat_m3p_db.fish
+++
@@ -1,3 +0,0 @@
-function aws_uat_m3p_db --description "SSH tunnel to AWS uat database"
-    ssh_tunnel ~/.ssh/uat_m3p.rsa 5433 m3p-uat.cluster-ro-cbm6meio8xhm.us-west-2.rds.amazonaws.com:5432 ec2-user@44.225.104.92
-end
