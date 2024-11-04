1114a70e3e7e97eee715150e4c4733b7095de61d
Michael Holtzscher <michael@holtzscher.com>
2024-11-03 21:47:41 -0600

    Remove .config/nix-darwin/modules/home-manager/files/nvim/LICENSE Remove .config/nix-darwin/modules/home-manager/files/nvim/README.md Remove .config/nix-darwin/modules/home-manager/files/nvim/.gitignore Remove .config/nix-darwin/modules/home-manager/files/nvim/.neoconf.json Remove .config/nix-darwin/modules/home-manager/files/nvim/init.lua Remove .config/nix-darwin/modules/home-manager/files/nvim/lazy-lock.json Remove .config/nix-darwin/modules/home-manager/files/nvim/lazyvim.json Remove .config/nix-darwin/modules/home-manager/files/nvim/dot_gitignore Remove .config/nix-darwin/modules/home-manager/files/nvim/dot_neoconf.json Remove .config/nix-darwin/modules/home-manager/files/nvim/lua/config/autocmds.lua Remove .config/nix-darwin/modules/home-manager/files/nvim/lua/config/keymaps.lua Remove .config/nix-darwin/modules/home-manager/files/nvim/lua/config/lazy.lua Remove .config/nix-darwin/modules/home-manager/files/nvim/lua/config/options.lua Remove .config/nix-darwin/modules/home-manager/files/nvim/lua/plugins/aerial.lua Remove .config/nix-darwin/modules/home-manager/files/nvim/lua/plugins/avante.lua Remove .config/nix-darwin/modules/home-manager/files/nvim/lua/plugins/buf.lua Remove .config/nix-darwin/modules/home-manager/files/nvim/lua/plugins/chatgpt.lua Remove .config/nix-darwin/modules/home-manager/files/nvim/lua/plugins/codesnap.lua Remove .config/nix-darwin/modules/home-manager/files/nvim/lua/plugins/colorscheme.lua Remove .config/nix-darwin/modules/home-manager/files/nvim/lua/plugins/dashboard.lua Remove .config/nix-darwin/modules/home-manager/files/nvim/lua/plugins/drop.lua Remove .config/nix-darwin/modules/home-manager/files/nvim/lua/plugins/gleam/init.lua Remove .config/nix-darwin/modules/home-manager/files/nvim/lua/plugins/go/init.lua Remove .config/nix-darwin/modules/home-manager/files/nvim/lua/plugins/java/init.lua Remove .config/nix-darwin/modules/home-manager/files/nvim/lua/plugins/vim-tmux-navigator.lua Remove .config/nix-darwin/modules/home-manager/files/nvim/stylua.toml

--- dot_config/nix-darwin/modules/home-manager/files/nvim/lua/plugins/aerial.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "stevearc/aerial.nvim",
-  opts = {
-    highlight_on_hover = true,
-    autojump = true,
-    -- open_automatic = true,
-    layout = { min_width = 50, default_direction = "prefer_left" },
-  },
-}
