f3ad4e2249311ff06e5b94d87749b680186367a8
Michael Holtzscher <michael@holtzscher.com>
2024-11-17 09:52:07 -0600

    Update .config/nix-darwin/modules/home-manager/programs/nushell.nix

--- dot_config/nix-darwin/modules/home-manager/programs/nushell.nix
+++ dot_config/nix-darwin/modules/home-manager/programs/nushell.nix
@@ -5,7 +5,13 @@
       enable = true;
       extraConfig = builtins.readFile ../files/nushell/functions.nu;
       shellAliases = {
-        build = "./gradlew build --parallel";
+        # General aliases
+        ll = "ls -al";
+        c = "clear";
+        lg = "lazygit";
+        pbj = "pbpaste | jq";
+
+        # chezmoi aliases
         ch = "chezmoi";
         chad = "chezmoi add";
         chrad = "chezmoi re-add";
@@ -13,10 +19,19 @@
         chd = "chezmoi diff";
         chda = "chezmoi data";
         chs = "chezmoi status";
+
+        # nix aliases
+        nup = "darwin-rebuild switch --flake ~/.config/nix-darwin";
+        nfu = "nix flake update";
+        ngc = "nix-collect-garbage -d";
+
+        # work aliases
+        aws_ecr_login = "aws ecr get-login-password | docker login --username AWS --password-stdin 188442536245.dkr.ecr.us-west-2.amazonaws.com";
+        # aws_export_envs = "export (aws configure export-credentials --profile $AWS_PROFILE --format env-no-export )";
+        aws_local = "env AWS_PROFILE=localstack aws --endpoint-url=http://localhost.localstack.cloud:4566";
+
+        build = "./gradlew build --parallel";
         gradle = "./gradlew";
-        ll = "ls -al";
-        c = "clear";
-        lg = "lazygit";
         sso = "aws_change_profile";
       };
     };
