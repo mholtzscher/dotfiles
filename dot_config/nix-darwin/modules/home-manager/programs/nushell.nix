e995db525b2579458e0dcb70d06dcbc6ecc6e114
Michael Holtzscher <michael@holtzscher.com>
2024-11-17 08:56:29 -0600

    Update .config/nix-darwin/modules/home-manager/files/nushell/functions.nu Update .config/nix-darwin/modules/home-manager/programs/nushell.nix Update .config/nvim/lazy-lock.json

--- dot_config/nix-darwin/modules/home-manager/programs/nushell.nix
+++ dot_config/nix-darwin/modules/home-manager/programs/nushell.nix
@@ -8,11 +8,12 @@
         build = "./gradlew build --parallel";
         ch = "chezmoi";
         chad = "chezmoi add";
+        chrad = "chezmoi re-add";
         chap = "chezmoi apply";
         chd = "chezmoi diff";
         chda = "chezmoi data";
         chs = "chezmoi status";
-        fmt = "./gradlew spotlessApply --parallel";
+        # fmt = "./gradlew spotlessApply --parallel";
         gradle = "./gradlew";
         ll = "ls -al";
         c = "clear";
