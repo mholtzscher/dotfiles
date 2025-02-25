a042267fed221ca37e5812e111f903764b41898d
Michael Holtzscher <michael@holtzscher.com>
2025-02-25 16:47:40 -0600

    Update .config/nvim/lazy-lock.json Update .config/nvim/lazyvim.json Update .config/nvim/lua/plugins/kulala.lua

--- dot_config/nvim/lua/plugins/kulala.lua
+++ dot_config/nvim/lua/plugins/kulala.lua
@@ -1,1 +1,6 @@
-return { "mistweaverco/kulala.nvim", opts = {} }
+return {
+  -- "mistweaverco/kulala.nvim",
+  -- opts = {},
+  -- -- keys = { "<leader>Rs", "<leader>Ra", "<leader>Ro" },
+  -- ft = { "http", "rest" },
+}
