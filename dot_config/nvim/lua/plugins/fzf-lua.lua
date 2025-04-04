0c95a571271e854cda2ac71da43f4635e29722fd
Michael Holtzscher <michael@holtzscher.com>
2025-04-04 09:34:50 -0500

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugins/avante.lua Update .config/nvim/lua/plugins/colorscheme.lua Update .config/nvim/lua/plugins/fzf-lua.lua

--- dot_config/nvim/lua/plugins/fzf-lua.lua
+++ dot_config/nvim/lua/plugins/fzf-lua.lua
@@ -1,8 +1,9 @@
 return {
-  "ibhagwan/fzf-lua",
-  -- optional for icon support
-  dependencies = { "nvim-tree/nvim-web-devicons", "echasnovski/mini.icons" },
-  -- or if using mini.icons/mini.nvim
-  -- dependencies = { "echasnovski/mini.icons" },
-  opts = {},
+  -- I don't know why I added this...
+  -- "ibhagwan/fzf-lua",
+  -- -- optional for icon support
+  -- dependencies = { "nvim-tree/nvim-web-devicons", "echasnovski/mini.icons" },
+  -- -- or if using mini.icons/mini.nvim
+  -- -- dependencies = { "echasnovski/mini.icons" },
+  -- opts = {},
 }
