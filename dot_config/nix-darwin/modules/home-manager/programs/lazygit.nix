87ea69d34bb6a65bd8630689e67c3e85c64f546a
Michael Holtzscher <michael@holtzscher.com>
2025-04-04 11:11:31 -0500

    Update .config/nix-darwin/modules/home-manager/files/zellij.kdl Update .config/nix-darwin/modules/home-manager/programs/lazygit.nix

--- dot_config/nix-darwin/modules/home-manager/programs/lazygit.nix
+++ dot_config/nix-darwin/modules/home-manager/programs/lazygit.nix
@@ -4,20 +4,48 @@
     lazygit = {
       enable = true;
       settings = {
-        gui.theme = {
-          activeBorderColor = [
-            "#f5c2e7"
-            "bold"
-          ];
-          inactiveBorderColor = [ "#a6adc8" ];
-          optionsTextColor = [ "#89b4fa" ];
-          selectedLineBgColor = [ "#313244" ];
-          selectedRangeBgColor = [ "#313244" ];
-          cherryPickedCommitBgColor = [ "#45475a" ];
-          cherryPickedCommitFgColor = [ "#f5c2e7" ];
-          unstagedChangesColor = [ "#f38ba8" ];
-          defaultFgColor = [ "#cdd6f4" ];
-          searchingActiveBorderColor = [ "#f9e2af" ];
+        gui = {
+          nerdFontsVersion = "3"; # String value
+          theme = {
+            # List containing two strings
+            activeBorderColor = [
+              "#ff9e64"
+              "bold"
+            ];
+
+            # List containing one string
+            inactiveBorderColor = [ "#27a1b9" ];
+
+            # List containing two strings
+            searchingActiveBorderColor = [
+              "#ff9e64"
+              "bold"
+            ];
+
+            # List containing one string
+            optionsTextColor = [ "#7aa2f7" ];
+
+            # List containing one string
+            selectedLineBgColor = [ "#283457" ];
+
+            # List containing one string
+            cherryPickedCommitFgColor = [ "#7aa2f7" ];
+
+            # List containing one string
+            cherryPickedCommitBgColor = [ "#bb9af7" ];
+
+            # List containing one string
+            markedBaseCommitFgColor = [ "#7aa2f7" ];
+
+            # List containing one string
+            markedBaseCommitBgColor = [ "#e0af68" ];
+
+            # List containing one string
+            unstagedChangesColor = [ "#db4b4b" ];
+
+            # String value (Note: Original YAML had this as a string, not a list)
+            defaultFgColor = [ "#c0caf5" ];
+          };
         };
       };
     };
