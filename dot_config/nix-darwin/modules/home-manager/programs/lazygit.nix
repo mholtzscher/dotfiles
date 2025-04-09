40a6ae2db64e57e1e2372729b5c97c75da3bed65
Michael Holtzscher <michael@holtzscher.com>
2025-04-08 22:47:42 -0500

    Update .config/nix-darwin/modules/home-manager/programs/lazygit.nix

--- dot_config/nix-darwin/modules/home-manager/programs/lazygit.nix
+++ dot_config/nix-darwin/modules/home-manager/programs/lazygit.nix
@@ -5,45 +5,24 @@
       enable = true;
       settings = {
         gui = {
-          nerdFontsVersion = "3"; # String value
+          nerdFontsVersion = "3";
           theme = {
-            # List containing two strings
             activeBorderColor = [
               "#ff9e64"
               "bold"
             ];
-
-            # List containing one string
             inactiveBorderColor = [ "#27a1b9" ];
-
-            # List containing two strings
             searchingActiveBorderColor = [
               "#ff9e64"
               "bold"
             ];
-
-            # List containing one string
             optionsTextColor = [ "#7aa2f7" ];
-
-            # List containing one string
             selectedLineBgColor = [ "#283457" ];
-
-            # List containing one string
             cherryPickedCommitFgColor = [ "#7aa2f7" ];
-
-            # List containing one string
             cherryPickedCommitBgColor = [ "#bb9af7" ];
-
-            # List containing one string
             markedBaseCommitFgColor = [ "#7aa2f7" ];
-
-            # List containing one string
             markedBaseCommitBgColor = [ "#e0af68" ];
-
-            # List containing one string
             unstagedChangesColor = [ "#db4b4b" ];
-
-            # String value (Note: Original YAML had this as a string, not a list)
             defaultFgColor = [ "#c0caf5" ];
           };
         };
