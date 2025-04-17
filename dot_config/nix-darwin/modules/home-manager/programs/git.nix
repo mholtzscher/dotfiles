55af37a9066e55bd875bb9bb77b73cfc9dcc7d5d
Michael Holtzscher <michael@holtzscher.com>
2025-04-16 21:47:57 -0500

    Update .config/nix-darwin/modules/home-manager/programs/git.nix Update .config/nix-darwin/modules/home-manager/programs/lazygit.nix Update .config/nvim/lazy-lock.json

--- dot_config/nix-darwin/modules/home-manager/programs/git.nix
+++ dot_config/nix-darwin/modules/home-manager/programs/git.nix
@@ -10,8 +10,13 @@
       signing.key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILyAn4X7ZQUyU7DQYHjQ4qccecA2r6ekk+jS1gjBfWAi";
       # signing.signByDefault = true;
       lfs.enable = true;
-      delta.enable = true;
-      includes = [ { path = ../themes/catppuccin.gitconfig; } ];
+      delta = {
+        enable = true;
+        options = {
+          # dark = true;
+        };
+      };
+      # includes = [ { path = ../themes/catppuccin.gitconfig; } ];
     };
   };
 }
--- dot_config/nix-darwin/modules/home-manager/programs/lazygit.nix
+++ dot_config/nix-darwin/modules/home-manager/programs/lazygit.nix
@@ -4,6 +4,10 @@
     lazygit = {
       enable = true;
       settings = {
+        commitLength = {
+          show = false;
+        };
+
         gui = {
           nerdFontsVersion = "3";
           theme = {
@@ -26,6 +30,30 @@
             defaultFgColor = [ "#c0caf5" ];
           };
         };
+
+        customCommands = [
+          {
+            key = "O";
+            description = "Open Pull Request with GitHub CLI";
+            context = "global";
+            command = "gh pr create -df && gh pr view --web";
+            loadingText = "Creating Pull Request";
+          }
+          # {
+          #   key = "<c-o>";
+          #   description = "Advanced - Open Pull Request with GitHub CLI";
+          #   context = "global";
+          #   prompts = [
+          #     {
+          #       type = "input";
+          #       title = "Jira Ticket";
+          #       key = "JiraTicket";
+          #     }
+          #   ];
+          #   command = "gh pr create -df && gh pr view --web";
+          #   loadingText = "Creating Pull Request";
+          # }
+        ];
       };
     };
   };
