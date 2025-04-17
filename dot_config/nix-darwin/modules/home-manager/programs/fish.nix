7ef945ca750fbd939032d5996d62d395645361d4
Michael Holtzscher <michael@holtzscher.com>
2025-04-17 14:46:20 -0500

    Update .config/nix-darwin/modules/home-manager/programs/fish.nix Update .config/nix-darwin/modules/home-manager/programs/yazi.nix

--- dot_config/nix-darwin/modules/home-manager/programs/fish.nix
+++ dot_config/nix-darwin/modules/home-manager/programs/fish.nix
@@ -345,6 +345,18 @@
           body = "curl wttr.in $argv";
           description = "get weather";
         };
+
+        y = {
+          body = ''
+            set tmp (mktemp -t "yazi-cwd.XXXXXX")
+            yazi $argv --cwd-file="$tmp"
+            if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+              builtin cd -- "$cwd"
+            end
+            rm -f -- "$tmp"
+          '';
+          description = "yazi";
+        };
       };
     };
   };
--- dot_config/nix-darwin/modules/home-manager/programs/yazi.nix
+++ dot_config/nix-darwin/modules/home-manager/programs/yazi.nix
@@ -1,10 +1,27 @@
 {
+  pkgs,
   ...
 }:
+let
+  yazi-flavors = pkgs.fetchFromGitHub {
+    owner = "BennyOe";
+    repo = "tokyo-night.yazi";
+    rev = "main";
+    sha256 = "sha256-+wZzxLPCttJ2WoDdI89sQ+CcZSFIA44HshxMoh4rJIs=";
+  };
+in
 {
   programs = {
     yazi = {
       enable = true;
+      flavors = {
+        tokyonight = "${yazi-flavors}";
+      };
+      theme = {
+        flavor = {
+          dark = "tokyonight";
+        };
+      };
     };
   };
 }
