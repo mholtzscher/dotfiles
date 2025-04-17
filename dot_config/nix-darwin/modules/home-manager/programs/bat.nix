cd36b9c8b0ff87697f55b6bf2e074f162891e0d3
Michael Holtzscher <michael@holtzscher.com>
2025-04-17 14:53:18 -0500

    Update .config/nix-darwin/modules/home-manager/programs/bat.nix Update .config/nvim/lazy-lock.json

--- dot_config/nix-darwin/modules/home-manager/programs/bat.nix
+++ dot_config/nix-darwin/modules/home-manager/programs/bat.nix
@@ -7,9 +7,18 @@
     bat = {
       enable = true;
       config = {
-        theme = "catppuccin-mocha";
+        theme = "tokyo-night";
       };
       themes = {
+        tokyo-night = {
+          src = pkgs.fetchFromGitHub {
+            owner = "folke";
+            repo = "tokyonight.nvim";
+            rev = "054790b8676d0c561b22320d4b5ab3ef175f7445";
+            sha256 = "sha256-mriZ9QBe1QIDsBkGd+tmg4bNFtD0evuSom2pWyQ1yEM=";
+          };
+          file = "extras/sublime/tokyonight_night.tmTheme";
+        };
         catppuccin-mocha = {
           src = pkgs.fetchFromGitHub {
             owner = "catppuccin";
