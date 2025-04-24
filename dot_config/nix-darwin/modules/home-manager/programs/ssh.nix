440a8c9bfd4db1901712326f79161f119f902f4a
Michael Holtzscher <michael@holtzscher.com>
2025-04-23 23:21:10 -0500

    Add .config/nix-darwin/modules/home-manager/programs/ssh.nix

---
+++ dot_config/nix-darwin/modules/home-manager/programs/ssh.nix
@@ -0,0 +1,35 @@
+{ ... }:
+{
+  programs = {
+    ssh = {
+      enable = true;
+      matchBlocks = {
+        mina-nas = {
+          identityFile = "~/.ssh/mina-nas-id_ed25519.pub";
+          identitiesOnly = true;
+          hostname = "10.69.69.156";
+          user = "root";
+        };
+
+        max-nas = {
+          identityFile = "~/.ssh/max-nas-id_ed25519.pub";
+          identitiesOnly = true;
+          hostname = "10.69.69.186";
+          user = "root";
+        };
+
+        wanda = {
+          identityFile = "~/.ssh/wanda-id_ed25519.pub";
+          identitiesOnly = true;
+          hostname = "10.69.69.60";
+          user = "michael";
+        };
+      };
+
+      extraConfig = ''
+        Host *
+          IdentityAgent "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
+      '';
+    };
+  };
+}
