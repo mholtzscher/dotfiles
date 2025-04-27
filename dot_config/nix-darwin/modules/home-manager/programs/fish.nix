7c0c0f15186e1ac1bd5b1b657fbb31b629df5513
Michael Holtzscher <michael@holtzscher.com>
2025-04-27 15:37:05 -0500

    Update .config/nix-darwin/flake.lock Update .config/nix-darwin/modules/home-manager/programs/fish.nix Update .config/nvim/lazy-lock.json

--- dot_config/nix-darwin/modules/home-manager/programs/fish.nix
+++ dot_config/nix-darwin/modules/home-manager/programs/fish.nix
@@ -334,6 +334,29 @@ in
           '';
           description = "yazi";
         };
+
+        zoxide_register_children = {
+          body = ''
+            set -f target_dir (prompt_pwd)
+
+            echo "Scanning for child directories in: $target_dir"
+            set -l count 0
+            fd --type d --maxdepth 1 . | while read -L child_dir
+                if test -n "$child_dir" # Ensure read command got a non-empty string
+                    echo "  Adding: $child_dir"
+                    zoxide add "$child_dir"
+                    set count (math $count + 1)
+                end
+            end
+
+            if test $count -eq 0
+                echo "No child directories found to add."
+            else
+                echo "Done. Added $count child directories to zoxide."
+            end
+          '';
+          description = "Adds immediate child directories of the current directory to zoxide's database.";
+        };
       };
     };
   };
