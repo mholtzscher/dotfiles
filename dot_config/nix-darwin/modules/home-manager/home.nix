384b3108a58b4769bb819194c06af4c8bad934df
Michael Holtzscher <michael@holtzscher.com>
2025-04-27 21:36:03 -0500

    Update .config/nix-darwin/modules/home-manager/home.nix Update .config/nix-darwin/modules/home-manager/programs/starship.nix

--- dot_config/nix-darwin/modules/home-manager/home.nix
+++ dot_config/nix-darwin/modules/home-manager/home.nix
@@ -6,6 +6,12 @@ let
     rev = "057ef5d260c1931f1dffd0f052c685dcd14100a3";
     sha256 = "sha256-1xZhQR1BhH2eqax0swlNtnPWIEUTxSOab6sQ3Fv9WQA=";
   };
+  starship = pkgs.fetchFromGitHub {
+    owner = "starship";
+    repo = "starship";
+    rev = "ed87dc5750338d37bfc2c17568ae3a9b589a8e8e";
+    sha256 = "sha256-kPvyCKf62x+hXAxL5+sdWHLVoUx/n96EFEBOTpXnQhw=";
+  };
 in
 {
   imports = [
@@ -23,6 +29,10 @@ in
     "${config.xdg.configHome}/1Password/ssh/agent.toml".source = ./files/1password-agent.toml;
     "${config.xdg.configHome}/fish/functions/update.fish".source = ./files/fish/functions/update.fish;
     "Library/Application Support/eza/theme.yml".source = "${tokyonight}/extras/eza/tokyonight.yml";
+    # "${config.xdg.configHome}/starship.toml".source =
+    #   "${starship}/docs/public/presets/toml/pure-preset.toml";
+    "${config.xdg.configHome}/starship.toml".source =
+      "${starship}/docs/public/presets/toml/jetpack.toml";
   };
 
   home.sessionVariables = {
