e1438cdd816059a5ef0e7d94e33befe5e88bd2ad
Michael Holtzscher <michael@holtzscher.com>
2025-08-01 22:45:19 -0500

    Update .config/nvim/lazy-lock.json Update .config/nvim/lazyvim.json Update .config/nvim/lua/plugins/fff.lua

--- dot_config/nvim/lua/plugins/fff.lua
+++ dot_config/nvim/lua/plugins/fff.lua
@@ -5,13 +5,13 @@ return {
     -- pass here all the options
   },
   keys = {
-    {
-      "<leader>ff", -- try it if you didn't it is a banger keybinding for a picker
-      function()
-        require("fff").toggle()
-      end,
-      desc = "Toggle FFF",
-    },
+    -- {
+    --   "<leader>ff", -- try it if you didn't it is a banger keybinding for a picker
+    --   function()
+    --     require("fff").toggle()
+    --   end,
+    --   desc = "Toggle FFF",
+    -- },
     {
       "<leader><leader>", -- try it if you didn't it is a banger keybinding for a picker
       function()
