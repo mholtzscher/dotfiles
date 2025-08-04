3778f76f442311400daa48b18300c19adc5cdc6c
Michael Holtzscher <michael@holtzscher.com>
2025-08-04 08:14:55 -0500

    Update .config/nvim/lua/plugins/fzf-lua.lua

--- dot_config/nvim/lua/plugins/fzf-lua.lua
+++ dot_config/nvim/lua/plugins/fzf-lua.lua
@@ -1,7 +1,10 @@
-return {}
---   {
---   -- this is needed fro octo.nvim to work. there is open pr for snacks to work
---   "ibhagwan/fzf-lua",
---   dependencies = { "nvim-tree/nvim-web-devicons", "echasnovski/mini.icons" },
---   opts = {},
--- }
+return {
+  -- this is needed fro octo.nvim to work. there is open pr for snacks to work
+  "ibhagwan/fzf-lua",
+  dependencies = { "echasnovski/mini.icons" },
+  opts = {
+    files = {
+      formatter = "path.filename_first",
+    },
+  },
+}
