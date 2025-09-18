d6680ba729776392b74de53a9cde6ac06521f19e
Michael Holtzscher <michael@holtzscher.com>
2025-09-18 08:16:10 -0500

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugins/colorscheme.lua

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -4,13 +4,13 @@ return {
     lazy = true,
     opts = { style = "night" },
   },
-  {
-    "vague2k/vague.nvim",
-    lazy = false,
-    priority = 1000,
-  },
-  {
-    "adibhanna/forest-night.nvim",
-    priority = 1000,
-  },
+  -- {
+  --   "vague2k/vague.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  -- },
+  -- {
+  --   "adibhanna/forest-night.nvim",
+  --   priority = 1000,
+  -- },
 }
