1d319c3b8acc48183af9d67db0808e03bbda021c
Michael Holtzscher <michael@holtzscher.com>
2025-09-19 13:34:57 -0500

    Add .config/nix-darwin/modules/home-manager/files/raycast/toggle-aerospace.sh

---
+++ dot_config/nix-darwin/modules/home-manager/files/raycast/toggle-aerospace.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Toggle Aerospace
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.description Toggle Aerospace Enabled
+# @raycast.author mholtzscher
+# @raycast.authorURL https://raycast.com/mholtzscher
+
+aerospace enable toggle
